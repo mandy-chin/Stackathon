cfc13c593f848bf59939cea65d4298f33eb58deb
mandy-chin <chin.mandyy@gmail.com>
2023-03-09 19:41:31 -0500

    backend is working

--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
+  entry: ['./src/index.js'],
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index_bundle.js',
+    path: __dirname + '/public',
+    filename: 'bundle.js',
   },
-  target: 'web',
-  devServer: {
-    port: '3000',
-    static: {
-      directory: path.join(__dirname, 'public')
-},
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.json'],
-  },
+  context: __dirname,
+  devtool: 'source-map',
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, 
-        exclude: /node_modules/, 
-        use: 'babel-loader', 
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
       },
       {
         test: /\.css$/i,
@@ -34,9 +25,45 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html')
-    })
-  ]
 };
+
+
+// module.exports = {
+//   entry: './src/index.js',
+//   mode: 'development',
+//   output: {
+//     path: __dirname + '/public',
+//     filename: "bundle.js"
+//   },
+//   target: 'web',
+//   devServer: {
+//     port: '3000',
+//     static: {
+//       directory: path.join(__dirname, 'public')
+// },
+//     open: true,
+//     hot: true,
+//     liveReload: true,
+//   },
+  // resolve: {
+  //   extensions: ['.js', '.jsx', '.json'],
+  // },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(js|jsx)$/, 
+  //       exclude: /node_modules/, 
+  //       use: 'babel-loader', 
+  //     },
+  //     {
+  //       test: /\.css$/i,
+  //       use: ['style-loader', 'css-loader', 'postcss-loader'],
+  //     },
+  //   ],
+  // },
+  // plugins: [
+  //   new HtmlWebpackPlugin({
+  //     template: path.join(__dirname, 'public', 'index.html')
+  //   })
+  // ]
+// };
